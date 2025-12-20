7844db7e33c93d9ce04ca158d35ac48b64d80753
tsybulin.y <maltaerman2@gmail.com>
2025-12-20 22:46:24 +0200

    fix(): fix double route issue

--- src/components/layout/ToolBar.vue
+++ src/components/layout/ToolBar.vue
@@ -1,26 +1,13 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue';
-import { useRouter } from 'vue-router';
-
 import { PAGE_NAME_ENUM } from '@/router'
 
 import BaseTabs from '@/components/bases/tabs/BaseTabs.vue';
-
-const router = useRouter();
 
 const tabs = [
   { id: PAGE_NAME_ENUM.MAIN, label: PAGE_NAME_ENUM.MAIN  },
   { id: PAGE_NAME_ENUM.HISTORY, label: PAGE_NAME_ENUM.HISTORY  },
   { id: PAGE_NAME_ENUM.SETTINGS, label: PAGE_NAME_ENUM.SETTINGS  },
 ];
-
-const tabId = ref(null);
-
-watchEffect(() => {
-  if (tabId.value) {
-    router.push({ name: tabId.value })
-  }
-})
 </script>
 
 <template>
@@ -28,6 +15,6 @@ watchEffect(() => {
     class="h-(--toolbarHeight) w-full"
     v-bind="{ tabs }"
     size="lg"
-    @change-active-tab="({ id }) => tabId = id"
+    @change-active-tab="({ id }) => $router.push({ name: id })"
   />
 </template>
