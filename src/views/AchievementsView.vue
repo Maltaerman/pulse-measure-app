9818bb7d87b109aec231db5d0f31ebf7278ac698
tsybulin.y <maltaerman2@gmail.com>
2025-12-20 14:48:08 +0200

    feat(): main page

---
+++ src/views/AchievementsView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <p
+      class="text-lg font-semibold col-span-2 text-neutral-800"
+      v-text="'Achievements view'"
+    />
+  </div>
+</template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { PAGE_NAME_ENUM } from '@/router';
+
+import BaseButton from '@/components/bases/BaseButton.vue';
+
+const ROUTES = [
+  { label: 'Measure', to: PAGE_NAME_ENUM.MEASURE, },
+  { label: 'History', to: PAGE_NAME_ENUM.HISTORY, },
+  { label: 'Achievements', to: PAGE_NAME_ENUM.ACHIEVEMENTS, },
+  { label: 'Settings', to: PAGE_NAME_ENUM.SETTINGS, },  
+];
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+
+    <h1
+      class="text-lg font-semibold col-span-2 text-neutral-800"
+      v-text="`Let's get started`"
+    />
+
+    <div class="grid grid-cols-2 gap-4">
+      <BaseButton
+        v-for="{ label, to } in ROUTES"
+        :key="label"
+        class="aspect-square size-full"
+        @click="$router.push({ name: to })"
+      >
+        {{ label }}
+      </BaseButton>
+    </div>
+  </div>
+</template>
