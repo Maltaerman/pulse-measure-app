457bee7db36f843b14092d7cf77129f42134fbda
tsybulin.y <maltaerman2@gmail.com>
2026-03-26 17:53:12 +0200

    style(): measure list item redesign

--- src/components/measure-list/MeasureList.vue
+++ src/components/measure-list/MeasureList.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
+import { format } from "date-fns";
+
 import MeasureListItem from './MeasureListItem.vue';
 import { useMeasure } from '@/composables/useMeasure';
 
 const { measureList, getMeasureList } = useMeasure();
 
-// getMeasureList();
+getMeasureList();
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
-    <MeasureListItem
+    <div
       v-for="measureListItem in measureList"
       :key="measureListItem.id"
-      v-bind="measureListItem"
-    />
+      class="flex flex-col gap-2"
+    >
+      <p 
+        class="text-text-primary font-bold text-lg transition-colors duration-300"
+        v-text="format(measureListItem.createdAt, 'MMMM d, yyyy')"
+      />
+
+      <MeasureListItem
+        v-bind="measureListItem"
+      />
+    </div>
   </div>
 </template>
