fde1e083034e0d7cc16a63e0f3660c2495c43aa6
tsybulin.y <maltaerman2@gmail.com>
2026-03-26 17:35:48 +0200

    style(): app header

---
+++ src/components/layout/DefaultHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts" />
+
+<template>
+  <header class="bg-bg/80 backdrop-blur-md border-b border-border flex flex-row justify-between items-center py-2 px-4 transition-colors duration-300">
+
+    <button class="text-text-secondary hover:text-text-primary">
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2">
+        <path d="M4 6h16M4 12h16M4 18h16"/>
+      </svg>
+    </button>
+
+    <h1 class="text-lg font-bold text-text-primary transition-colors duration-300">
+      Heart<span class="text-primary">Rate</span>
+    </h1>
+
+    <button class="text-text-secondary hover:text-text-primary">
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2">
+        <path d="M12 8a4 4 0 100 8 4 4 0 000-8zm8 4a8 8 0 01-.34 2.3l2.04 1.58-2 3.46-2.42-.98a8.03 8.03 0 01-2 1.15l-.36 2.54H9.08l-.36-2.54a8.03 8.03 0 01-2-1.15l-2.42.98-2-3.46 2.04-1.58A8 8 0 014 12c0-.8.12-1.57.34-2.3L2.3 8.12l2-3.46 2.42.98a8.03 8.03 0 012-1.15L9.08 2h5.84l.36 2.54a8.03 8.03 0 012 1.15l2.42-.98 2 3.46-2.04 1.58c.22.73.34 1.5.34 2.3z"/>
+      </svg>
+    </button>
+
+  </header>
+</template>
--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
+
+import DefaultHeader from '@/components/layout/DefaultHeader.vue';
+
+
 import ToolBar from '@/components/layout/ToolBar.vue';
 import CookieBanner from '@/components/layout/CookieBanner.vue';
 
@@ -9,13 +13,15 @@ const isCookieBannerShown = ref(false);
 
 <template>
   <div>
+    <DefaultHeader class="sticky top-0 z-50 h-(--headerHeight)" />
+
     <main class="relative flex size-full flex-1 flex-col --screenMinHeight transition-colors duration-300 p-2 bg-bg">
       <slot />
     </main>
 
 
-    <ToolBar class="fixed bottom-0" />
+    <!-- <ToolBar class="fixed bottom-0" /> -->
 
-    <CookieBanner v-if="isCookieBannerShown" />
+    <!-- <CookieBanner v-if="isCookieBannerShown" /> -->
   </div>
 </template>
