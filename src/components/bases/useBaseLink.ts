9858bbe9de698f3c5ef42d6ad716515f82f18bfd
tsybulin.y <maltaerman2@gmail.com>
2026-04-03 22:37:47 +0300

    fix(): link cleanup

--- src/components/bases/useBaseLink.ts
+++
@@ -1,8 +0,0 @@
-// import { computed, type Ref } from 'vue'
-
-// // TODO: add logic
-// export function useLink(propsData: Ref<any>) {
-//   const to = computed(() => propsData.value.to ?? null)
-
-//   return { to, isActive: false }
-// }
