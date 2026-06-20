5adaab1ce5f3f44983c670a095549f443fb0e108
tsybulin.y <maltaerman2@gmail.com>
2026-06-20 16:19:48 +0300

    style(): route animation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { useLayout } from '@/layout/useLayout'
 import { useEventListener } from '@vueuse/core'
-// import { useLayoutRouteTransition } from '@/layout/useLayoutRouteTransition';
 import { usePopupManager, useUser } from '@/composables'
 
 const { layoutComponent } = useLayout()
 const { popupKeydownEventListener } = usePopupManager()
 const { setUser } = useUser()
-// const { layoutRouteTransition } = useLayoutRouteTransition();
 
 useEventListener('keydown', popupKeydownEventListener)
 
@@ -16,13 +14,13 @@ setUser()
 
 <template>
   <component :is="layoutComponent">
-    <!-- <Transition
-      :name="layoutRouteTransition"
+    <Transition
+      name="transition-fade"
       mode="out-in"
-    > -->
-    <router-view :key="$route.fullPath" v-slot="{ Component, route }">
-      <component :is="Component" :key="route.name" />
-    </router-view>
-    <!-- </Transition> -->
+    >
+      <router-view :key="$route.fullPath" v-slot="{ Component, route }">
+        <component :is="Component" :key="route.name" />
+      </router-view>
+    </Transition>
   </component>
 </template>
